119232554f3ad7715d10b02baa028363443b03be
Dan Barker <poozzab@udel.edu>
2014-03-09 18:42:33 -0400

    DAL/DAO for Profile manipulation

---
+++ src/vendor/dal/ProfileDAL.java
@@ -0,0 +1,13 @@
+package vendor.dal;
+
+public class ProfileDAL{
+
+	public ProfileDAO getProfileInformation(String someInput){
+		ProfileDAO profileInfo new ProfileDAO();
+
+		/* Add the Access Logic Here! */
+
+		return profileInfo;
+	}
+
+}
---
+++ src/vendor/dao/ProfileDAO.java
@@ -0,0 +1,18 @@
+package vendor.dao;
+
+public class ProfileDAO{
+
+	private String[] theDeets;
+	
+	public ProfileDAO(){}
+
+	public String[] getDetails(){
+		return theDeets;
+	}
+
+	public void setDetails(String[] newDeets){
+		System.out.println("No thanks.");
+	}
+
+
+}
