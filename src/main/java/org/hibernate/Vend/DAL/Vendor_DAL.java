35e054aabe6540dc3b5d4a211828579f7399eb66
dlake413 <biglake@udel.edu>
2014-04-14 18:13:59 -0400

    Adding Vendor_DAL

---
+++ src/main/java/org/hibernate/Vend/DAL/Vendor_DAL.java
@@ -0,0 +1,62 @@
+package org.hibernate.Vend.DAL;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Vend.Vendor;
+import org.hibernate.Vend.util.HibernateUtil;
+
+public class Vendor_DAL {
+	
+	public String Insert(Vendor vendor){
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		
+		Vendor input = new Vendor();
+		
+		input.setVendorName(vendor.getVendorName());
+		input.setActiveFlg(vendor.isActiveFlg());
+		
+		session.save(input);
+		session.getTransaction().commit();
+		
+		return input.getVendorID();		
+	}
+	
+	public List<Vendor> Select(String Id){
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		
+		Query query = session.createQuery("from Vendor where VendorID = :id").setParameter("id", Id);
+		
+		List<Vendor> list = query.list();
+		
+		session.getTransaction().commit();
+		return list;
+	}
+	
+	public void Update(Vendor vendor){
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		
+		Vendor find = (Vendor) session.get(Vendor.class, vendor.getVendorID());
+		
+		if(find != null){
+			Query query= session.createQuery("UPDATE Vendor SET vendorname= :vn, activeflg = :fl " + 
+					"WHERE VendorId= :id")
+					.setParameter("vn", vendor.getVendorName())
+					.setParameter("fl", vendor.isActiveFlg())
+					.setParameter("id", vendor.getVendorID());
+			query.executeUpdate();
+		}
+		
+		session.getTransaction().commit();
+	}
+	
+	public void Delete(Vendor vendor){
+		vendor.setActiveFlg(false);
+		Update(vendor);
+	}
+
+}
--- src/main/java/org/hibernate/Vend/VendorManagement.java
+++ src/main/java/org/hibernate/Vend/VendorManagement.java
@@ -1,12 +1,44 @@
 package org.hibernate.Vend;
 
+import java.util.List;
+
 import org.hibernate.Session;
+import org.hibernate.Vend.DAL.Vendor_DAL;
 import org.hibernate.Vend.util.HibernateUtil;
 
 public class VendorManagement {
 	
 	public static void main(String arg[]){
 		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		
+		Vendor_DAL vendal = new Vendor_DAL();
+		
+		Vendor vendor = new Vendor();
+		
+		vendor.setActiveFlg(true);
+		vendor.setVendorName("DUKE MAGICAL PARK");
+	
+		String id = vendal.Insert(vendor);
+		
+		System.out.println("Insert Successful! VendorID: " + id);
+		
+		vendor = vendal.Select(id).get(0);
+		
+		System.out.println(vendor.getVendorID() + ' ' + vendor.getVendorName() + ' ' + vendor.isActiveFlg());
+		
+		vendor.setVendorName("Duke Wizard Park");
+		
+		vendal.Update(vendor);
+		
+		vendor = vendal.Select(id).get(0);
+		
+		System.out.println(vendor.getVendorID() + ' ' + vendor.getVendorName() + ' ' + vendor.isActiveFlg());
+		
+		vendal.Delete(vendor);
+		
+		vendor = vendal.Select(id).get(0);
+		
+		System.out.println(vendor.getVendorID() + ' ' + vendor.getVendorName() + ' ' + vendor.isActiveFlg());
 		
 		session.getSessionFactory().close();
 	}
